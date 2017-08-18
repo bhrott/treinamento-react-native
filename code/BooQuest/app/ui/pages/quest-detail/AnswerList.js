3e0b762c19303cec4b13e91c51bcd85ab66e997d
Ben-hur Santos Ott <ben-hur@cwi.com.br>
2017-08-18 18:10:13 -0300

    feat(answer-list): adding base list

--- code/BooQuest/app/ui/pages/quest-detail/AnswerList.js
+++ code/BooQuest/app/ui/pages/quest-detail/AnswerList.js
@@ -5,7 +5,8 @@ import {
     StyleSheet,
     Dimensions,
     Animated,
-    Image
+    Image,
+    FlatList
 } from 'react-native'
 import PropTypes from 'prop-types'
 import { Text } from 'boo-ui/components'
@@ -48,23 +49,76 @@ export default class AnswerList extends React.Component {
         this.props.onRequestClose()
     }
 
+    _renderHeader() {
+        return (
+            <TouchableOpacity 
+                onPress={this._requestClose.bind(this)}
+                style={headerStyle.container}>
+                <Image 
+                    source={require('./img/back.png')}
+                    style={headerStyle.imageBack}/>
+                <Text
+                    numberOfLines={1} 
+                    style={headerStyle.text}>
+                    {this.props.quest.title}
+                </Text>
+                <View style={headerStyle.divisor}></View>
+            </TouchableOpacity>
+        )
+    }
+
+    _renderBody() {
+        const bodyStyle = StyleSheet.create({
+            container: {
+                flex: 1,
+                width: '100%'
+            },
+            list: {
+
+            }
+        })
+
+        const listStyles = StyleSheet.create({
+            container: {
+                width: '90%'
+            }
+        })
+
+        return (
+            <View style={bodyStyle.container}>
+                <FlatList
+                    style={listStyles.container}
+                    data={[]}
+                    keyExtractor={(item, index) => { return item.key }}
+                    renderItem={this._renderListItem.bind(this)}
+                />
+            </View>
+        )
+    }
+
+    _renderListItem({ item }) {
+        const listItemStyle = StyleSheet.create({
+            container: {
+                height: 100,
+                width: '100%',
+                borderColor: ColorPalette.grayLight,
+                borderBottomWidth: 1
+            }
+        })
+
+        return (
+            <View style={listItemStyle.container}>
+
+            </View>
+        )
+    }
+
     render() {
         return (
             <Animated.View style={[styles.container, { top: this.containerTopPosition }]}>
                 <View style={styles.content}>
-                    <TouchableOpacity 
-                        onPress={this._requestClose.bind(this)}
-                        style={headerStyle.container}>
-                        <Image 
-                            source={require('./img/back.png')}
-                            style={headerStyle.imageBack}/>
-                        <Text
-                            numberOfLines={1} 
-                            style={headerStyle.text}>
-                            {this.props.quest.title}
-                        </Text>
-                        <View style={headerStyle.divisor}></View>
-                    </TouchableOpacity>
+                    {this._renderHeader()}
+                    {this._renderBody()}
                 </View>
             </Animated.View>
         )
@@ -118,7 +172,7 @@ const headerStyle = StyleSheet.create({
     imageBack: {
         position: 'absolute',
         top: 15,
-        right: 20,
+        left: 20,
         transform:[{rotate: '-90 deg'}]
     }
 })
